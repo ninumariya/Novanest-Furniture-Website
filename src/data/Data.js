9c0f8a64862aab591afd3dadfd99b57a75dbd324
Ninumariya <ninumariya2518@gmail.com>
2025-04-13 15:01:08 +0400

    Add project Novanest-A furniture website

---
+++ src/data/Data.js
@@ -0,0 +1,329 @@
+export const navbar = [
+  {
+    id: 1,
+    path: "/",
+    nav: "Home",
+  },
+  {
+    id: 2,
+    path: "/shop",
+    nav: "Shop",
+  },
+  {
+    id: 3,
+    path: "/blog",
+    nav: "Blog",
+  },
+  {
+    id: 4,
+    path: "/about",
+    nav: "About Us",
+  },
+  {
+    id: 5,
+    path: "/contact",
+    nav: "Contact Us",
+  },
+];
+
+export const banners = [
+  {
+    banner: "../images/novanest/slider-1.jpeg",
+  },
+  {
+    banner: "../images/novanest/slider-2.jpeg",
+  },
+  {
+    banner: "../images/novanest/slider-3.jpeg",
+  },
+];
+
+export const category = [
+  {
+    id: 1,
+    img: "../images/novanest/living_room.jpg",
+    name: "Living Room",
+  },
+  {
+    id: 2,
+    img: "../images/novanest/bed_room.jpg",
+    name: "Bed Room",
+  },
+  {
+    id: 3,
+    imgs: [
+      {
+        id: 31,
+        img: "../images/novanest/kids_room.jpg",
+        name: "Kids Room",
+      },
+      {
+        id: 32,
+        img: "../images/novanest/kitchen.jpg",
+        name: "Kitchen",
+      },
+    ],
+  },
+];
+
+export const products = [
+  {
+    id: 1,
+    img: "../images/novanest/chair/1.jpeg",
+    category: "bestsell",
+    brand: "Old Brand",
+    title: "Natural Wood Dining Chair",
+    short_description: "Lorem ipsum dolor",
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 2,
+    img: "../images/novanest/chair/2.jpeg",
+    category: "New Category",
+    brand: "Latest Brand",
+    title: "Dining Chair",
+    short_description: "Lorem ipsum dolor",
+    price: "1209.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 3,
+    img: "../images/novanest/chair/3.jpeg",
+    category: "bestsell",
+    brand: "Latest Brand",
+    title: "Best Chair",
+    short_description: "Lorem ipsum dolor",
+    price: "529.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 4,
+    img: "../images/novanest/chair/4.jpeg",
+    category: "Old Category",
+    brand: "Latest Brand",
+    title: "Natural Wood Chair",
+    short_description: "Lorem ipsum dolor",
+    price: "900.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 5,
+    img: "../images/novanest/chair/5.jpeg",
+    category: "bestsell",
+    brand: "Latest Brand",
+    title: "Sitting Chair",
+    short_description: "Lorem ipsum dolor",
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 6,
+    img: "../images/novanest/chair/6.jpeg",
+    category: "Low Price",
+    brand: "Latest Brand",
+    title: "Natural Wood Dining Chair",
+    short_description: "Lorem ipsum dolor",
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+
+  {
+    id: 7,
+    img: "../images/novanest/chair/7.jpeg",
+    category: "bestsell",
+    brand: "Latest Brand",
+    title: "Beautiful Chair",
+    short_description: "Lorem ipsum dolor",
+    price: "1129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 8,
+    img: "../images/novanest/chair/8.jpeg",
+    category: "Low Price",
+    brand: "Indian",
+    title: "Chair set",
+    short_description: "Lorem ipsum dolor",
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 9,
+    img: "../images/novanest/chair/9.jpeg",
+    category: "Best One",
+    brand: "Indian",
+    title: "Best One",
+    short_description: "Lorem ipsum dolor",
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 10,
+    img: "../images/novanest/chair/10.jpeg",
+    category: "High Price",
+    brand: "Indian",
+    title: "Chair",
+    short_description: "Lorem ipsum dolor",
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 11,
+    img: "../images/novanest/chair/11.jpeg",
+    category: "bestsell",
+    brand: "Indian",
+    title: "Old Chair",
+    short_description: "Lorem ipsum dolor",
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 12,
+    img: "../images/novanest/chair/12.jpeg",
+    category: "High Price",
+    brand: "Indian",
+    title: "New Chair",
+    short_description: "Lorem ipsum dolor",
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 13,
+    img: "../images/novanest/chair/13.jpeg",
+    category: "bestsell",
+    brand: "Indian",
+    title: "New Brand Chair",
+    short_description: "Lorem ipsum dolor",
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+  {
+    id: 14,
+    img: "../images/novanest/chair/14.jpeg",
+    category: "High Price",
+    brand: "Indian",
+    title: "Stylish Chair",
+    short_description: "Lorem ipsum dolor",
+    price: "129.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elitLorem ipsum dolor sit amet, consectetur adipiscing elit  euismod er  elit",
+  },
+];
+
+export const arriveItems = [
+  {
+    id: 1,
+    img: "../images/novanest/chair/insta-1.jpg",
+  },
+  {
+    id: 2,
+    img: "../images/novanest/chair/insta-2.jpg",
+  },
+  {
+    id: 3,
+    img: "../images/novanest/chair/insta-3.jpg",
+  },
+  {
+    id: 4,
+    img: "../images/novanest/chair/insta-4.jpg",
+  },
+  {
+    id: 5,
+    img: "../images/novanest/chair/insta-5.jpg",
+  },
+  {
+    id: 6,
+    img: "../images/novanest/chair/insta-6.jpg",
+  },
+];
+
+export const offer = [
+  {
+    customer_img: "../images/novanest/creation.jpg",
+    title: "CRAFT OWN FURNITURE",
+    subtitle: "Your new forever favorites are here",
+  },
+];
+
+export const brands = [
+  {
+    id: 1,
+    img: "../images/novanest/chair/1.jpeg",
+    title: "Coty",
+    short_description: "Grab a deal Airspun Products",
+  },
+  {
+    id: 2,
+    img: "../images/novanest/chair/2.jpg",
+    title: "Laten",
+    short_description: "Grab a deal Airspun Products",
+  },
+  {
+    id: 3,
+    img: "../images/novanest/chair/3.png",
+    title: "Imagic",
+    short_description: "Grab a deal Airspun Products",
+  },
+  {
+    id: 4,
+    img: "../images/novanest/chair/4.png",
+    title: "Pee Safe",
+    short_description: "Grab a deal Airspun Products",
+  },
+  {
+    id: 5,
+    img: "../images/novanest/chair/5.jpg",
+    title: "Maybeleine",
+    short_description: "Grab a deal Airspun Products",
+  },
+  {
+    id: 6,
+    img: "../images/novanest/chair/6.png",
+    title: "The Purest",
+    short_description: "Grab a deal Airspun Products",
+  },
+];
+
+export const footer = [
+  {
+    id: 1,
+    header: "Our Store",
+    content1:
+      "novanest is one of the biggest international fashion companies, one of the world’s largest distribution groups.",
+  },
+  {
+    id: 2,
+    header: "Quick Links",
+    content1: "My Account",
+    content2: "Shopping Cart",
+    content3: "Wishlist",
+    content4: "Product Compare",
+  },
+  {
+    id: 3,
+    header: "Information",
+    content1: "Privacy Policy",
+    content2: "Refund Policy",
+    content3: "Shipping & Return",
+    content4: "Terms & Condition",
+  },
+  {
+    id: 4,
+    header: "Let's Get In Touch",
+    content1: "Sign Up For Our newsletter and receive 10% off your",
+  },
+];
